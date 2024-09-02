47190d0f7d79bb902b25f3bc70bf451dee611838
Jyothish Kumar <JyothishElanjimattom@gmail.com>
2024-09-02 12:51:48 +0530

    Mapped

---
+++ src/test/java/steps/loginSteps.java
@@ -0,0 +1,11 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+
+public class loginSteps {
+
+	@Given("User should navigate to the application")
+	public void userShouldNavigate() {
+		System.out.println("i am here");
+	}
+}
