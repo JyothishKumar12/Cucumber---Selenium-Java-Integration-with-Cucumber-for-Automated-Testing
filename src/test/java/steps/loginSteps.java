6a86f2b300ca21b6a429ae8e36b257baf700c6fc
Jyothish Kumar <JyothishElanjimattom@gmail.com>
2024-09-08 12:04:08 +0530

    added parametrization and removed duplicate code

--- src/test/java/steps/loginSteps.java
+++ src/test/java/steps/loginSteps.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -30,16 +31,28 @@ public class loginSteps {
 	    driver.findElement(By.xpath("//span[contains(text(),' Login ')]")).click();
 	    		}
 
-	@Given("User enter the username as Ortoni")
-	public void userEnterTheUsernameAsOrtoni() {
-	    driver.findElement(By.id("mat-input-0")).sendKeys("bkcart");
-	}
+//	@Given("User enter the username as bkcart")
+//	public void userEnterTheUsernameAsBkcart() {
+//	    driver.findElement(By.id("mat-input-0")).sendKeys("bkcart");
+//	}
+//
+//	@Given("User enter the password as Bkcart45")
+//	public void userEnterThePasswordAsOrtoni() {
+//	driver.findElement(By.id("mat-input-1")).sendKeys("Bkcart45");
+//	}
 
-	@Given("User enter the password as Ortoni")
-	public void userEnterThePasswordAsOrtoni() {
-	driver.findElement(By.id("mat-input-1")).sendKeys("Bkcart45");
-	}
 
+@Given("User enter the username as {string}")
+public void userEnterTheUsernameAs(String string) {
+	 driver.findElement(By.id("mat-input-0")).sendKeys(string);
+}
+
+@Given("User enter the password as {string}")
+public void userEnterThePasswordAs(String string) {
+	driver.findElement(By.id("mat-input-1")).sendKeys(string);
+   
+}
+	
 	@When("User click the login button")
 	public void userClickTheLoginButton() throws InterruptedException {
 		Thread.sleep(3000);
@@ -47,9 +60,32 @@ public class loginSteps {
 	}
 
 	@Then("login should be successful")
-	public void loginShouldBeSuccessful() {
-	String login =   driver.findElement(By.xpath("(//span[@class='mdc-button__label']/child::span)[1]")).getText();
+	public void loginShouldBeSuccessful() throws InterruptedException {
+		Thread.sleep(3000);	
+		String login =   driver.findElement(By.xpath("(//span[@class='mdc-button__label']/child::span)[1]")).getText();
 	System.out.println(login);
 	}
+	
+	
+//@Given("User enter the username as bkca")
+//public void userEnterTheUsernameAsbkca() {
+//driver.findElement(By.id("mat-input-0")).sendKeys();
+//}
+//
+//	
+//
+//
+//	
+//	@Given("User enter the password as Bkca")
+//	public void userEnterThePasswordAsBkca() {
+//		driver.findElement(By.id("mat-input-1")).sendKeys();
+//	}
+
+	@Then("login should be Fail")
+	public void loginShouldBeFail() {
+	 String message =   driver.findElement(By.id("mat-mdc-error-0")).getText();
+	 Assert.assertEquals(message,"Username or Password is incorrect.");
+	}
+
 
 }
